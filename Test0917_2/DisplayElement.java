cb5fb7be755e1b720f0eb1bc0726c5852e469856
beefplz <swlblue@naver.com>
2023-11-30 21:52:27 +0900

    commit

---
+++ Test0917_2/DisplayElement.java
@@ -0,0 +1,5 @@
+package Test0917_2;
+
+public interface DisplayElement {
+    public void display();
+}
---
+++ Test0917_2/Observer.java
@@ -0,0 +1,5 @@
+package Test0917_2;
+
+public interface Observer {
+    public void update(float temp, float humidity, float pressure);
+}
